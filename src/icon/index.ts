a304c4e04a2eae93e9d038f5b8461f39122ff696
ring-wdr <akswnd55@gmail.com>
2024-07-01 23:35:35 +0900

    about page 기초

--- src/icon/index.ts
+++ src/icon/index.ts
@@ -1,6 +1,7 @@
 import AlarmIcon from './alarm.svg';
+import ChevronRightIcon from './chevronRight.svg';
 import HeartIcon from './heart.svg';
 import SearchIcon from './search.svg';
 import UserIcon from './user.svg';
 
-export { AlarmIcon, HeartIcon, SearchIcon, UserIcon };
+export { AlarmIcon, ChevronRightIcon, HeartIcon, SearchIcon, UserIcon };
