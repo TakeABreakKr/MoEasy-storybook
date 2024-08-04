bb9929beaae6986e06ab9fa7434597eeea4565a0
ring-wdr <akswnd55@gmail.com>
2024-08-04 16:03:13 +0900

    card to compount pattern

--- ui/card/card.tsx
+++ ui/card/card.tsx
@@ -1,7 +1,4 @@
 import { HTMLAttributes } from 'react';
-import clsx from 'clsx';
-
-import Image from 'next/image';
 
 import { Button } from '../button/button';
 import { EllipsisIcon } from '../icon';
@@ -10,17 +7,25 @@ import { NameTag } from '../tag';
 
 import { magic } from '../../utils/styles/index.css';
 import * as cardStyle from './card.css';
+import {
+  CardDescription,
+  CardMember,
+  CardMembers,
+  CardThumbnail,
+  CardTitle,
+  CardTrigger,
+  CardTriggerItem,
+  CardWrapper,
+} from './compound-card';
 
 export type CardProps = {
-  idx: number;
+  idx: number | string;
   title: string;
   description?: string;
   thumbnail?: string;
   count?: number;
   maxCount?: number;
-  isLiked?: boolean;
-  isWaiting?: boolean;
-  onLikeClick?: () => void;
+  members?: CardMember[];
 } & HTMLAttributes<HTMLDivElement>;
 
 export default function Card({
@@ -29,45 +34,29 @@ export default function Card({
   className,
   description = '',
   count = 0,
-  isWaiting,
   maxCount = 10,
-  onLikeClick,
+  members = [],
   ...props
 }: CardProps) {
   return (
-    <div className={clsx(cardStyle.card, className)} {...props}>
-      <div className={cardStyle.thumbnailWrapper}>
-        <div className={cardStyle.thumbnail}>
-          <Image src={`https://via.placeholder.com/116/${idx}`} width={116} height={116} alt={title} />
-        </div>
-      </div>
-      <div className={cardStyle.interact}>
-        <Button size="small" variant="ghost" rounded="small" className={magic}>
-          <EllipsisIcon />
-        </Button>
-      </div>
+    <CardWrapper data-meeting-index={idx} {...props}>
+      <CardThumbnail src={`https://via.placeholder.com/116/${idx}`} alt={title} />
+      <CardTrigger>
+        <CardTriggerItem align="center">수정</CardTriggerItem>
+        <CardTriggerItem align="center">초대하기</CardTriggerItem>
+        <CardTriggerItem align="center" notice>
+          탈퇴하기
+        </CardTriggerItem>
+        <CardTriggerItem align="center" notice>
+          삭제
+        </CardTriggerItem>
+      </CardTrigger>
       <div>
-        <h2 className={cardStyle.title}>{title}</h2>
-        <pre className={cardStyle.description}>{description}</pre>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
       </div>
       <Separator direction="horizontal" color="#d5d5d5" />
-      <div className={cardStyle.memberWrapper}>
-        <NameTag userRole="limit">5명</NameTag>
-        <NameTag src={'https://via.placeholder.com/30'} userRole="admin">
-          모임장
-        </NameTag>
-        <NameTag src={'https://via.placeholder.com/30'} userRole="manager">
-          매니저
-        </NameTag>
-        <NameTag src={'https://via.placeholder.com/30'} userRole="manager">
-          매니저
-        </NameTag>
-        <NameTag src={'https://via.placeholder.com/30'}>모임원</NameTag>
-        <NameTag src={'https://via.placeholder.com/30'}>모임원</NameTag>
-        <NameTag src={'https://via.placeholder.com/30'}>모임원</NameTag>
-        <NameTag src={'https://via.placeholder.com/30'}>모임원</NameTag>
-        <NameTag>더 보기</NameTag>
-      </div>
-    </div>
+      <CardMembers members={members} />
+    </CardWrapper>
   );
 }
